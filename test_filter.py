ee9bc1d93a461076a1905e4aa1eb0109858dfe8a
tmr <myemail-tmr@protonmail.com>
2020-01-21 23:55:53 +0800

    add t.co

--- test_filter.py
+++ test_filter.py
@@ -8,6 +8,8 @@ def test_filter_url():
     assert filter_url("https://www.douban.com/annual/2019#test") == "https://www.douban.com/annual/2019"
     # twitter
     assert filter_url("https://twitter.com/gentilkiwi/status/1217731204373499904?s=12") == "https://twitter.com/gentilkiwi/status/1217731204373499904"
+    # t.co
+    assert filter_url("https://t.co/WEJLVgyuRX?ssr=true") == "https://t.co/WEJLVgyuRX"
     # bilibili
     assert filter_url("https://m.bilibili.com/bangumi/play/ep285192?share_source=more&share_medium=ipad&bbid=Z442BB442087877E463990432E2A2E386DE0&ts=1575632820") == "https://m.bilibili.com/bangumi/play/ep285192"
     assert filter_url("https://www.bilibili.com/bangumi/play/ep285192?share_source=more&share_medium=ipad&bbid=Z442BB442087877E463990432E2A2E386DE0&ts=1575632820") == "https://www.bilibili.com/bangumi/play/ep285192"
